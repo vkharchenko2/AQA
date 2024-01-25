d576b028f6ca37f15a6b564ad6b08bbbe73f0556
VOLODYMYR KHARCHENKO <v.kharchenko996@gmail.com>
2024-01-25 21:14:26 +0100

    Moved task 1 to a new branch

---
+++ src/test/java/HomeWorkApp.java
@@ -0,0 +1,45 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 6;
+        int b = 2;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 2;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value < 0 && value >= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 9;
+        int b = 3;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
